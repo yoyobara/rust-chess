31c771e5b2329e9709730e7ab0a899f70cc5a83f
yoyobara <yotambarak6@gmail.com>
2024-04-17 22:18:34 +0300

    started to implement chess engine

--- src/chess.rs
+++ src/chess.rs
@@ -1,5 +1,14 @@
+use crate::chessboard::ChessBoard;
 
 /*
  * a struct which represents and manages a game of chess.
  */
-struct ChessEngine {}
+struct ChessEngine {
+    white_player_name: String,
+    black_player_name: String,
+    chessboard: ChessBoard
+}
+
+impl ChessEngine {
+
+}
