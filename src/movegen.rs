c45ddda1134f1000fc58f63b83ce7237621bd237
Yotam Barak <yotambarak6@gmail.com>
2025-10-14 18:05:37 +0300

    feat(movegen.rs): knight

--- src/movegen.rs
+++ src/movegen.rs
@@ -97,7 +97,34 @@ pub fn get_knight_pseudo_legal_moves(
     src_square: Square,
     piece: Piece,
 ) -> Vec<Move> {
-    unimplemented!()
+    const KNIGHT_MOVES: [(i8, i8); 8] = [
+        (-1, 2),
+        (-1, -2),
+        (1, 2),
+        (1, -2),
+        (-2, 1),
+        (-2, -1),
+        (2, 1),
+        (2, -1),
+    ];
+
+    let mut moves = Vec::new();
+
+    for (i, j) in KNIGHT_MOVES {
+        let Some(sq) = src_square.get_relative_square(i, j) else {
+            continue;
+        };
+
+        if let Some(target_piece) = board.get(sq) {
+            if target_piece.piece_color != piece.piece_color {
+                moves.push(Move::new(src_square, sq, None));
+            }
+        } else {
+            moves.push(Move::new(src_square, sq, None));
+        }
+    }
+
+    moves
 }
 
 pub fn get_rook_pseudo_legal_moves(
