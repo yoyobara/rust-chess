418fd49bcdfa78ecc40a5c5254f58b8f7972cfe9
Yotam Barak <yotambarak6@gmail.com>
2025-10-14 20:07:52 +0300

    feat(movegen.rs): king

--- src/movegen.rs
+++ src/movegen.rs
@@ -172,5 +172,25 @@ pub fn get_king_pseudo_legal_moves(
     src_square: Square,
     piece: Piece,
 ) -> Vec<Move> {
-    unimplemented!()
+    let mut moves = Vec::new();
+
+    for i in -1..=1 {
+        for j in -1..=1 {
+            if i == 0 && j == 0 {
+                continue;
+            }
+
+            if let Some(dst) = src_square.get_relative_square(i, j) {
+                if let Some(target_piece) = board.get(dst) {
+                    if target_piece.piece_color != piece.piece_color {
+                        moves.push(Move::new(src_square, dst, None));
+                    }
+                } else {
+                    moves.push(Move::new(src_square, dst, None));
+                }
+            }
+        }
+    }
+
+    moves
 }
